6e2ae3ec42eff8e15188c12eea7e7765d079bd77
opiur <opiur@G60CYD3>
2024-11-12 20:27:39 +0200

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20241112_WorkWithFilesDemo
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Age { get; set; }
+    }
+}
